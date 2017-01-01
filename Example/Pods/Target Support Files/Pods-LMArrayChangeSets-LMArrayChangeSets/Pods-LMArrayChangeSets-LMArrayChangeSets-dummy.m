f052ce3df56081394e0653fb60ce6ba207fe9cfc
cdann <cdann@lintmachine.com>
2017-01-01 11:24:54 -0800

    Update podfile format. Share LMArrayChangeSets scheme for Carthage support.

--- Example/Pods/Target Support Files/Pods-LMArrayChangeSets-LMArrayChangeSets/Pods-LMArrayChangeSets-LMArrayChangeSets-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LMArrayChangeSets_LMArrayChangeSets : NSObject
-@end
-@implementation PodsDummy_Pods_LMArrayChangeSets_LMArrayChangeSets
-@end
